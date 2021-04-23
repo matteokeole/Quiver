d82b5d726c2adc815ed153a0e4a3007b1bf85d48
Mattéo <matteoo34@icloud.com>
2021-04-23 22:30:49 +0200

    new game inputs

--- index.php
+++ index.php
@@ -35,8 +35,26 @@
 				init: function() {
 					$("title").textContent = `Quiver ${Game.version}`; // updating the window title with the last version
 					Game.lang("en_US"); // setting the game language to english (USA) - can be modified in the options
-					document.querySelectorAll("input").forEach(function(e) {e.value = ""}); // resetting input values
-					$("main").style.display = "block" // opening the game window
+					// resetting input values
+					play.new_game.player_name.value = "";
+					play.new_game.game_name.value = "";
+					$("main").style.display = "block"; // opening the game window
+					play.new_game.player_name.addEventListener("keyup", function() {Game.check_input_value(play.new_game.player_name)});
+					play.new_game.game_name.addEventListener("keyup", function() {Game.check_input_value(play.new_game.game_name)});
+				},
+				rickroll: function() {alert("YOU'VE BEEN RICKROLLED :-)")},
+				check_input_value: function(input) {
+					if (/^\s+$/.test(input.value) || input.value.length === 0) {
+						// the input value is composed only of whitespaces or is empty
+						(input.getAttribute("id") === "player_name") ? player_name_ok = false : game_name_ok = false;
+						play.new_game.play.setAttribute("disabled", "disabled");
+						play.new_game.play.removeEventListener("click", Game.launch_new_game)
+					} else (input.getAttribute("id") === "player_name") ? player_name_ok = true : game_name_ok = true; // no errors
+					if (player_name_ok && game_name_ok) {
+						console.log("les deux sont ok")
+						play.new_game.play.removeAttribute("disabled");
+						play.new_game.play.addEventListener("click", Game.launch_new_game)
+					}
 				},
 				lang: function(l) {
 					// l: language name (str)
@@ -64,7 +82,9 @@
 						// new game section
 						play.new_game.subtitle.textContent = r["new_game.text"];
 						play.new_game.player_name_tip.textContent = r.new_game["player_name_tip.text"];
+						play.new_game.player_name.setAttribute("placeholder", r.new_game["player_name_placeholder.text"]);
 						play.new_game.game_name_tip.textContent = r.new_game["game_name_tip.text"];
+						play.new_game.game_name.setAttribute("placeholder", r.new_game["game_name_placeholder.text"]);
 						play.new_game.character_selection_tip.textContent = r.new_game["character_selection_tip.text"];
 						play.new_game.play.textContent = r["new_game.text"];
 						// launch backup section
@@ -359,7 +379,9 @@
 				new_game: {
 					subtitle: null,
 					player_name_tip: null,
+					player_name: null,
 					game_name_tip: null,
+					game_name: null,
 					character_selection_tip: null,
 					play: null
 				},
@@ -582,6 +604,9 @@
 				}
 			};
 
+			var player_name_ok = false,
+				game_name_ok = false;
+
 			function $(e) {return document.querySelector(e)}
 
 			function load() {
@@ -632,7 +657,9 @@
 				// new game section
 				play.new_game.subtitle = $(".new_game .subtitle");
 				play.new_game.player_name_tip = $(".new_game .player_name_tip");
+				play.new_game.player_name = $(".new_game #player_name");
 				play.new_game.game_name_tip = $(".new_game .game_name_tip");
+				play.new_game.game_name = $(".new_game #game_name");
 				play.new_game.character_selection_tip = $(".new_game .character_selection_tip");
 				play.new_game.play = $(".new_game .btn-new_game");
 				// launch backup section
@@ -675,7 +702,6 @@
 				document.querySelectorAll(".btn[data-function]").forEach(function(e) {
 					e.addEventListener("click", function() {Game.toggle_menu(this.getAttribute("data-target"), this.getAttribute("data-function"))})
 				});
-				play.new_game.play.addEventListener("click", function() {Game.launch_new_game()});
 				document.querySelector("#open_backup").onchange = Game.open_backup;
 				play.launch_backup.launch.addEventListener("click", function() {Game.launch_backup()});
 
